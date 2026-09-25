00d9167379ed7e0a9eee200caac4e8e54b22bf60
JayasreePochamireddy <jayasreepochamireddy@gmail.com>
2026-09-25 15:58:19 +0530

    Time: 7 ms (19.77%) | Memory: 47.2 MB (53.49%) - LeetSync

--- 463-island-perimeter/island-perimeter.java
+++ 463-island-perimeter/island-perimeter.java
@@ -3,7 +3,7 @@ class Solution {
         if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]==0) return 1;
         if(grid[i][j]==2) return 0; //land no perimeter
         grid[i][j] = 2;
-        if(grid[i][j]==1) return 0; 
+        if(grid[i][j]==1) return 1; 
         return dfs(grid,i-1,j) + dfs(grid,i+1,j) + dfs(grid,i,j-1) + dfs(grid,i,j+1);
 
     }
